733fe9095b722e2aecc4d98c986808820847de20
Eric McKinney <mckinney99@gmail.com>
2018-03-13 16:44:59 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
   get '/auth', to: 'sessions#create'
   post '/search', to: 'searches#foursquare'
+  get '/friends', to: 'searches#friends'
 
 end
